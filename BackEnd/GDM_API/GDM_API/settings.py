cac11d1ec6c88e5796add30426b4701ad0534ca8
Arian Hadi <arian.hadi78@gmail.com>
2021-07-09 01:40:57 +0430

    gdm_app initial

--- BackEnd/GDM_API/GDM_API/settings.py
+++ BackEnd/GDM_API/GDM_API/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'GDM_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gdm_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
