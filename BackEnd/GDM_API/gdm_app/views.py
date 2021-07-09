0a353dcb9e45f77ffcbcf53637609892435a7310
Arian Hadi <arian.hadi78@gmail.com>
2021-07-09 23:27:19 +0430

    projectview converted to viewset

--- BackEnd/GDM_API/gdm_app/views.py
+++ BackEnd/GDM_API/gdm_app/views.py
@@ -1,44 +1,83 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .serializers import ProjectSerializer
 from .models import Project, ProjectManager, TeamManager, TeamMember
+from gdm_app import serializers
+from django.http import Http404
 
 # Create your views here.
 
-class ProjectView(viewsets.ModelViewSet):
+class ProjectView(viewsets.ViewSet):
 
-    serializer_class = ProjectSerializer
-    #queryset = Project.objects.all()
-    def get_queryset(self):
-        total = set()
-        for e in ProjectManager.objects.filter(base_user__exact=self.request.user):
-            total.add(e.project)
-        for e in TeamManager.objects.filter(base_user__exact=self.request.user):
-            total.add(e.team.project)
-        for e in TeamMember.objects.filter(base_user__exact=self.request.user):
-            total.add(e.team.project)
-        return total
+    def list(self,request):
+        queryset = set()
+        context = {}
 
-
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
 
         for e in ProjectManager.objects.filter(base_user__exact=self.request.user):
+            queryset.add(e.project)
             if e.project.id in context:
                 context[e.project.id] += ["Project Manager"]
             else:
                 context[e.project.id] = ["Project Manager"]
+
+
+
         for e in TeamManager.objects.filter(base_user__exact=self.request.user):
+            queryset.add(e.team.project)
             if e.team.project.id in context:
                 context[e.team.project.id] += ["Team Manager"]
             else:
                 context[e.team.project.id] = ["Team Manager"]
+
+
+
         for e in TeamMember.objects.filter(base_user__exact=self.request.user):
+            queryset.add(e.team.project)
             if e.team.project.id in context:
                 context[e.team.project.id] += ["Team Member"]
             else:
                 context[e.team.project.id] = ["Team Member"]
-        return context
-            
-        
+
+
+        serializer = ProjectSerializer(queryset,context=context,many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = set()
+        context = {}
+
+        for e in ProjectManager.objects.filter(base_user__exact=self.request.user):
+            if e.project.id == int(pk):
+                queryset.add(e.project)
+                if e.project.id in context:
+                    context[e.project.id] += ["Project Manager"]
+                else:
+                    context[e.project.id] = ["Project Manager"]
+
+
+
+        for e in TeamManager.objects.filter(base_user__exact=self.request.user):
+            if e.team.project.id == int(pk):
+                queryset.add(e.team.project)
+                if e.team.project.id in context:
+                    context[e.team.project.id] += ["Team Manager"]
+                else:
+                    context[e.team.project.id] = ["Team Manager"]
+
+
+
+        for e in TeamMember.objects.filter(base_user__exact=self.request.user):
+            if e.team.project.id == int(pk):
+                queryset.add(e.team.project)
+                if e.team.project.id in context:
+                    context[e.team.project.id] += ["Team Member"]
+                else:
+                    context[e.team.project.id] = ["Team Member"]
+
+        if len(queryset) == 0:
+            raise Http404
+        serializer = ProjectSerializer(queryset,context=context,many=True)
+        return Response(serializer.data)
     
