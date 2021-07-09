28c4835a19ceb01b79196d9637987a8b585df7ed
mgh5225 <mgh.5225@gmail.com>
2021-07-10 00:46:50 +0430

    fix get_me

--- gdm_ui/src/store/actions/users.js
+++ gdm_ui/src/store/actions/users.js
@@ -1,12 +1,12 @@
 import * as types from "./types";
 import catchError from "../../hoc/catchError";
 
-import { users, auth } from "../../axios/main";
+import { auth } from "../../axios/main";
 
 export const get_me = (err) => {
   return catchError(async (dispatch) => {
-    const { data } = await users.get("/me");
-    dispatch({ type: types.set_me, me: data.me });
+    const { data } = await auth.get("/user");
+    dispatch({ type: types.set_me, me: data });
   }, err);
 };
 
