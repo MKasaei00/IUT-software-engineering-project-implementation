492bf61350378839cc1dee4239443283edf29691
Arian Hadi <arian.hadi78@gmail.com>
2021-07-09 17:30:16 +0430

    rest api initial

--- BackEnd/GDM_API/GDM_API/settings.py
+++ BackEnd/GDM_API/GDM_API/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'gdm_app.NormalUser'
 
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'gdm_app'
 ]
 
@@ -49,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'GDM_API.urls'
